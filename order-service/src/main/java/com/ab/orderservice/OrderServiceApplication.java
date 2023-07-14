437102fd261aff23fa726289ce1f75d6b44ae687
Alex Brackenridge <a.brack@live.co.uk>
2023-07-14 09:47:14 +0100

    All services now registered and running with eureka server.

--- order-service/src/main/java/com/ab/orderservice/OrderServiceApplication.java
+++ order-service/src/main/java/com/ab/orderservice/OrderServiceApplication.java
@@ -2,7 +2,7 @@ package com.ab.orderservice;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
-import org.springframework.cloud.netflix.eureka.EnableEurekaClient;
+//import org.springframework.cloud.netflix.eureka.EnableEurekaClient;
 
 @SpringBootApplication
 public class OrderServiceApplication {
--- product-service/src/main/java/com/ab/productservice/ProductServiceApplication.java
+++ product-service/src/main/java/com/ab/productservice/ProductServiceApplication.java
@@ -2,8 +2,8 @@ package com.ab.productservice;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
-import org.springframework.cloud.client.discovery.EnableDiscoveryClient;
-import org.springframework.cloud.netflix.eureka.EnableEurekaClient;
+//import org.springframework.cloud.client.discovery.EnableDiscoveryClient;
+//import org.springframework.cloud.netflix.eureka.EnableEurekaClient;
 
 @SpringBootApplication
 public class ProductServiceApplication {

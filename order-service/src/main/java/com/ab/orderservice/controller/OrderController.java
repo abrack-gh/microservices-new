7e778835f29563f1dc330aa726d5467c9d160014
Alex Brackenridge <a.brack@live.co.uk>
2023-07-18 20:10:20 +0100

    Initially implemented Actuator and Circuit breaker on Order controller class.

--- order-service/src/main/java/com/ab/orderservice/controller/OrderController.java
+++ order-service/src/main/java/com/ab/orderservice/controller/OrderController.java
@@ -2,6 +2,7 @@ package com.ab.orderservice.controller;
 
 import com.ab.orderservice.dto.OrderRequest;
 import com.ab.orderservice.service.OrderService;
+import io.github.resilience4j.circuitbreaker.annotation.CircuitBreaker;
 import lombok.RequiredArgsConstructor;
 import org.springframework.http.HttpStatus;
 import org.springframework.web.bind.annotation.*;
@@ -15,8 +16,16 @@ public class OrderController {
 
     @PostMapping
     @ResponseStatus(HttpStatus.CREATED)
+    @CircuitBreaker(name = "inventory", fallbackMethod = "fallbackMethod")
     public String placeOrder(@RequestBody OrderRequest orderRequest) {
         orderService.placeOrder(orderRequest);
         return "Order placed successfully";
     }
+
+    public String fallbackMethod(OrderRequest orderRequest, RuntimeException e){
+
+        return "Failure, try again later.";
+
+    }
+
 }
